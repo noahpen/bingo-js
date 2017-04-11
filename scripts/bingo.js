b43215c1770a81e7c07c7e7cb16de70fe5e9fac6
noahpen <noahpeneycad@hotmail.com>
2017-04-10 21:28:58 -0400

    Implemented number calling

--- scripts/bingo.js
+++ scripts/bingo.js
@@ -5,6 +5,23 @@
 
 // 75 possible numbers
 var usedNumbers = new Array(76);
+var calledNumbers = new Array();
+
+// winning combinations
+var winners = [
+			['sq0','sq1','sq2','sq3','sq4'],
+			['sq5','sq6','sq7','sq8','sq9'],
+			['sq10','sq11','sqfree','sq13','sq14'],
+			['sq15','sq16','sq17','sq18','sq19'],
+			['sq20','sq21','sq22','sq23','sq24'],
+			['sq0','sq5','sq10','sq15','sq20'],
+			['sq1','sq6','sq11','sq16','sq21'],
+			['sq2','sq7','sqfree','sq17','sq22'],
+			['sq3','sq8','sq13','sq18','sq23'],
+			['sq4','sq9','sq14','sq19','sq24'],
+			['sq0','sq6','sqfree','sq18','sq24'],
+			['sq4','sq8','sqfree','sq16','sq20']
+		];
 
 function init() {
     generateNewCard();
@@ -46,8 +63,8 @@ function generateNewNum() {
 
 function resetUsedNumbers() {
     // sets all elements of the usedNumbers array to false (resets the array)
-    for (var j = 0; j < usedNumbers.length; j++) {
-        usedNumbers[j] = false;
+    for (var i = 0; i < usedNumbers.length; i++) {
+        usedNumbers[i] = false;
     }
 }
 
@@ -55,6 +72,18 @@ function resetUsedNumbers() {
 function generateAnotherCard() {
     resetUsedNumbers();
     generateNewCard();
+    resetSquareColours();
+}
+
+// resets all squares except FREE to white
+function resetSquareColours() {
+    for (var i = 0; i < 25; i++) {
+        if (i == 12)
+            continue;
+        var currentSquare = document.getElementById("sq" + i);
+        currentSquare.style.backgroundColor = "#ffffff";
+    }
+    return;
 }
 
 function markSquare(square) {
@@ -66,5 +95,24 @@ function markSquare(square) {
     return;
 }
 
-
+function callNumber() {
+    var rand = Math.floor(Math.random() * 75) + 1; // random number between 1 and 75
+    // if the number is in the array (already been called)
+    if (calledNumbers.includes(rand))
+        callNumber();
+    else {
+        calledNumbers.push(rand);
+        if (rand >= 1 && rand <= 15)
+            document.getElementById("currentCall").innerHTML = 'B' + rand;
+        else if (rand >= 16 && rand <= 30)
+            document.getElementById("currentCall").innerHTML = 'I' + rand;
+        else if (rand >= 31 && rand <= 45)
+            document.getElementById("currentCall").innerHTML = 'N' + rand;
+        else if (rand >= 46 && rand <= 60)
+            document.getElementById("currentCall").innerHTML = 'G' + rand;
+        else
+            document.getElementById("currentCall").innerHTML = 'O' + rand;
+        document.getElementById("calledNums").innerHTML = calledNumbers;
+    } 
+}
 
